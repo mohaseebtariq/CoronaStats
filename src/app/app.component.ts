195c7c2d630acca898e4a1f3577bf3375de8ebee
mohaseebtariq <mo.haseeb.tariq@gmail.com>
2020-03-29 16:47:49 +0500

    made several changes is layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'CoranaLive';
+export class AppComponent implements OnInit {
+  title = "CoranaLive";
+  date;
+
+  ngOnInit() {
+    this.date = new Date();
+  }
 }
